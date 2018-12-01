4ee4ee6a3e32c97d7919ba728ccb36391af1c2fe
Jacob Patterson <jacob.patterson@gmail.com>
2018-11-30 21:30:29 -0800

    removing printPointDistances() from mouse listener, so clicking now only adds points.

--- src/main/java/com/github/ants280/compGeo/ui/CompGeoMouseListener.java
+++ src/main/java/com/github/ants280/compGeo/ui/CompGeoMouseListener.java
@@ -1,13 +1,8 @@
 package com.github.ants280.compgeo.ui;
 
-import com.github.ants280.compgeo.CompGeoUtils;
 import com.github.ants280.compgeo.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.Comparator;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class CompGeoMouseListener extends MouseAdapter
 {
@@ -21,35 +16,10 @@ public class CompGeoMouseListener extends MouseAdapter
 	@Override
 	public void mouseReleased(MouseEvent event)
 	{
-		Point point = PointUiUtils.getPoint(event);
-		switch (event.getButton())
+		if (event.getButton() == MouseEvent.BUTTON1) // left
 		{
-			case MouseEvent.BUTTON1: // left
-				frame.addPoints(point);
-				break;
-			case MouseEvent.BUTTON3: // right
-				this.printPointDistances(point);
-				break;
-			default:
-				break;
+			Point point = PointUiUtils.getPoint(event);
+			frame.addPoints(point);
 		}
-	}
-
-	private void printPointDistances(Point point)
-	{
-		System.out.printf("Distances from %s to points:%n", point);
-
-		frame.getCanvas().getPoints()
-				.stream()
-				.collect(Collectors.toMap(
-						Function.identity(),
-						otherPoint -> CompGeoUtils.getDistance(point, otherPoint)))
-				.entrySet()
-				.stream()
-				.sorted(Comparator.comparing(Map.Entry::getValue))
-				.forEach(entry -> System.out.printf(
-				"\t%s is at distance %.2f%n",
-				entry.getKey(),
-				entry.getValue()));
 	}
 }
