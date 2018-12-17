a7aa511309826b937d1501c098ebecedaad43e60
Jacob Patterson <jacob.patterson@gmail.com>
2018-12-16 16:48:52 -0800

    adding point equals() tests, formatting test file.

--- src/test/java/com/github/ants280/compgeo/PointTest.java
+++ src/test/java/com/github/ants280/compgeo/PointTest.java
@@ -1,6 +1,6 @@
 package com.github.ants280.compgeo;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class PointTest
@@ -12,7 +12,8 @@ public class PointTest
 		Point p1 = new Point(1, 0);
 
 		int actualCompareToValue = p0.compareTo(p1);
-		assertTrue("p0 should be < p1 because it has a larger x cordinate when the y coordinates are the same",
+		Assert.assertTrue(
+				"p0 should be < p1 because it has a larger x cordinate when the y coordinates are the same",
 				actualCompareToValue < 0);
 	}
 
@@ -23,7 +24,80 @@ public class PointTest
 		Point p1 = new Point(1, 0);
 
 		int actualCompareToValue = p0.compareTo(p1);
-		assertTrue("p0 should be > p1 because it has a larger x cordinate when the y coordinates are the same",
+		Assert.assertTrue(
+				"p0 should be > p1 because it has a larger x cordinate when the y coordinates are the same",
 				actualCompareToValue > 0);
+	}
+
+	@Test
+	public void testEquals_same()
+	{
+		Object p0 = new Point(0, 1);
+		Object p1 = p0;
+
+		boolean actualEquals = p0.equals(p1);
+		boolean expectedEquals = true;
+
+		Assert.assertEquals(expectedEquals, actualEquals);
+	}
+
+	@Test
+	public void testEquals_null()
+	{
+		Object p0 = new Point(0, 1);
+		Object p1 = null;
+
+		boolean actualEquals = p0.equals(p1);
+		boolean expectedEquals = false;
+
+		Assert.assertEquals(expectedEquals, actualEquals);
+	}
+
+	@Test
+	public void testEquals_wrongObject()
+	{
+		Object p0 = new Point(0, 1);
+		Object p1 = "(0.000000,1.000000)";
+
+		boolean actualEquals = p0.equals(p1);
+		boolean expectedEquals = true;
+
+		Assert.assertEquals(expectedEquals, actualEquals);
+	}
+
+	@Test
+	public void testEquals_differentX()
+	{
+		Object p0 = new Point(0, 1);
+		Object p1 = new Point(2, 1);
+
+		boolean actualEquals = p0.equals(p1);
+		boolean expectedEquals = false;
+
+		Assert.assertEquals(expectedEquals, actualEquals);
+	}
+
+	@Test
+	public void testEquals_differentY()
+	{
+		Object p0 = new Point(0, 1);
+		Object p1 = new Point(0, 2);
+
+		boolean actualEquals = p0.equals(p1);
+		boolean expectedEquals = false;
+
+		Assert.assertEquals(expectedEquals, actualEquals);
+	}
+
+	@Test
+	public void testEquals_eq()
+	{
+		Object p0 = new Point(0, 1);
+		Object p1 = new Point(0, 1);
+
+		boolean actualEquals = p0.equals(p1);
+		boolean expectedEquals = true;
+
+		Assert.assertEquals(expectedEquals, actualEquals);
 	}
 }
